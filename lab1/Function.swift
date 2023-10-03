5b47e3ecba7e1720ab2d1b9e5771294f8ace48ba
Marcin <mwijaszka99@gmail.com>
2023-10-03 16:58:24 +0200

    added view

---
+++ lab1/Function.swift
@@ -0,0 +1,8 @@
+//
+//  Function.swift
+//  lab1
+//
+//  Created by student on 03/10/2023.
+//
+
+import Foundation
---
+++ lab1/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  lab1
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
