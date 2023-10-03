d45647208de784af89506593e44099930cc48bcc
Marcin <mwijaszka99@gmail.com>
2023-10-03 16:28:31 +0200

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
